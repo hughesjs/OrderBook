9068ad9827c931576ef5c2b81949739f5433a24f
James Hughes <james@pyrosoftsolutions.co.uk>
2022-10-17 00:56:50 +0100

    Parellelised perf-tests and isolated them

--- src/perf-testing/entrypoint.sh
+++ src/perf-testing/entrypoint.sh
@@ -1,9 +1,3 @@
-# These should probably be parameterised
-# Ideally, mongo would be reinitialised between tests too
+mongoimport --uri mongodb://$MONGO_USER:$MONGO_PASS@$MONGO_HOST:$MONGO_PORT/OrderBooks --collection OnionFutures --type json --file ./onion-futures-seed.json --authenticationDatabase admin
 
-mongoimport --uri mongodb://mongo:mongo_pass@mongo:27017/OrderBooks --collection OnionFutures --type json --file ./onion-futures-seed.json --authenticationDatabase admin
-
-k6 run --vus 10 --duration 30s get-price-test.js > /get-price-test-results.json
-k6 run --vus 10 --duration 30s add-order-test.js > /add-order-test-results.json
-k6 run --vus 10 --duration 30s modify-order-test.js > /modify-order-test-results.json
-k6 run --vus 10 --duration 30s cycle-test.js > /cycle-test-results.json
+k6 run --vus 10 --duration 30s $TEST_FILE.js > /results.json
