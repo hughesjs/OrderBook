e5e8e105e67f487f143932b8c47b8ea0cfa765f2
James Hughes <james@pyrosoftsolutions.co.uk>
2022-10-17 01:58:51 +0100

    Results into github workflow is probably easier

--- src/perf-testing/entrypoint.sh
+++ src/perf-testing/entrypoint.sh
@@ -1,3 +1,3 @@
 mongoimport --uri mongodb://$MONGO_USER:$MONGO_PASS@$MONGO_HOST:$MONGO_PORT/OrderBooks --collection OnionFutures --type json --file ./onion-futures-seed.json --authenticationDatabase admin
 
-k6 run --vus 10 --duration 30s $TEST_FILE.js > /results.json
+k6 run --vus 10 --duration 30s $TEST_FILE.js
